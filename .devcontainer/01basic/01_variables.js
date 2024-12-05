5b537e5d53e31dbcf87093285a14c97203cc5067
ihatebulshit <imstranger000@gmail.com>
2024-12-05 22:21:48 +0000

    datatypeoperations

---
+++ .devcontainer/01basic/01_variables.js
@@ -0,0 +1,17 @@
+const acccountId = 144553
+let accountEmail = "test@test.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+
+
+//acccountId = 2
+
+/*
+Don't use var
+
+*/
+
+console.log(acccountId);
+console.table([acccountId,accountEmail,accountPassword,accountCity, accountState])
+
---
+++ .devcontainer/01basic/03_conversionOperation.js
@@ -0,0 +1,51 @@
+/*
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//Convertion result
+// "33" => 33
+// 33 => NaN
+//Ture => 1
+//False => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+*/
+//***************************Operations***********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "Hello"
+let str2 = "Amit"
+let str3 = str1 +" "+ str2
+
+console.log(str3);
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
